48d7a4f50a3ecbba42c06c5e8844bb09ff4766f9
Rishi Raj <rishiraj88+github@gmail.com>
2026-04-25 22:57:12 +0200

    Update the chatbot app and README file.

--- 04-langchain-gpt-flask-langchain/langchain-gpt-flask-langchain.py
+++ 04-langchain-gpt-flask-langchain/langchain-gpt-flask-langchain.py
@@ -1,29 +1,29 @@
-from flask import Flask, render_template, request, jsonify
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
+from flask import Flask, render_template, request, jsonify
 
 prompt = open('website_text.txt', 'r').read()
 
-hotel_assistant_template = prompt + """
+hotel_assistant__template = prompt + """
 You are the hotel manager of Landon Hotel, named "Mr. Landon". 
 Your expertise is exclusively in providing information and advice about anything related to Landon Hotel. 
 This includes any general Landon Hotel related queries. 
 You do not provide information outside of this scope. 
-If a question is not about Landon Hotel, respond with, "I can't assist you with that, sorry!" 
-Question: {question} 
+If a query is not about Landon Hotel, respond with, "I can't assist you with that, sorry!" 
+Query: {query} 
 Answer: 
 """
 
-hotel_assistant_prompt_template = PromptTemplate( 
-    input_variables=["question"], 
-    template=hotel_assistant_template 
+hotel_assistant__prompt_template = PromptTemplate( 
+    input_variables=["query"], 
+    template=hotel_assistant__template 
     ) 
 
 llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0) 
 
-llm_chain = hotel_assistant_prompt_template | llm 
+language_chain = hotel_assistant__prompt_template | llm 
 
-def query_llm(question): 
+def query_language_model(query): 
     response = llm_chain.invoke({'question': question}) 
     return response 
 
@@ -33,11 +33,11 @@ app = Flask(__name__)
 def index(): 
     return render_template("index.html") 
 
-@app.route("/chatbot", methods=["POST"]) 
+@app.route("/chat", methods=["POST"]) 
 def chatbot(): 
     data = request.get_json() 
-    question = data["question"] 
-    response = query_llm(question) 
+    query = data["query"] 
+    response = query_language_model(query) 
     return jsonify({"response": response}) 
 
 if __name__ == "__main__": 
