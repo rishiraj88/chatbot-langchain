6b62bcf3a55f9dbf7f51539b36989f0c2dd77fe3
Rishi Raj <rishiraj88+github@gmail.com>
2026-04-25 22:49:33 +0200

    Add a chatbot app with LangChain and Flask.

--- 05-chatbot-flask-langchain/chatbot-flask-langchain.py
+++ 05-chatbot-flask-langchain/chatbot-flask-langchain.py
@@ -1,34 +1,31 @@
+from flask import Flask, render_template, request, jsonify
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
-from flask import Flask, render_template, request, jsonify
 
 prompt = open('website_text.txt', 'r').read()
 
-hotel_assistant__template = prompt + """
+hotel_assistant_template = prompt + """
 You are the hotel manager of Landon Hotel, named "Mr. Landon". 
 Your expertise is exclusively in providing information and advice about anything related to Landon Hotel. 
 This includes any general Landon Hotel related queries. 
 You do not provide information outside of this scope. 
-If a query is not about Landon Hotel, respond with, "I can't assist you with that, sorry!" 
-Query: {query} 
+If a question is not about Landon Hotel, respond with, "I can't assist you with that, sorry!" 
+Question: {question} 
 Answer: 
 """
 
-hotel_assistant__prompt_template = PromptTemplate( 
-    input_variables=["query"], 
-    template=hotel_assistant__template 
+hotel_assistant_prompt_template = PromptTemplate( 
+    input_variables=["question"], 
+    template=hotel_assistant_template 
     ) 
 
 llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0) 
 
-language_chain = hotel_assistant__prompt_template | llm 
+llm_chain = hotel_assistant_prompt_template | llm 
 
-def query_language_model(query): 
-    print(language_chain.invoke({'query': query})) 
+def query_llm(question): 
+    response = llm_chain.invoke({'question': question}) 
     return response 
-
-# while True:
-    # query_language_model(input())
 
 app = Flask(__name__) 
 
@@ -36,11 +33,11 @@ app = Flask(__name__)
 def index(): 
     return render_template("index.html") 
 
-@app.route("/chat", methods=["POST"]) 
+@app.route("/chatbot", methods=["POST"]) 
 def chatbot(): 
     data = request.get_json() 
-    query = data["query"] 
-    response = query_language_model(query) 
+    question = data["question"] 
+    response = query_llm(question) 
     return jsonify({"response": response}) 
 
 if __name__ == "__main__": 
